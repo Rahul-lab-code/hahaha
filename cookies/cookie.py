77ba8e25b4e9eae24fdc428ef9ccb8e7c94f6ef8
Rahul <rahulkumarmutcherla@gmail.com>
2025-04-03 05:23:35 +0530

    Added removing an cookie

--- cookies/cookie.py
+++ cookies/cookie.py
@@ -12,7 +12,12 @@ def setCookie():
     res = make_response("<h1>Cookie is set</h1>")
     res.set_cookie("theme","dark")
     return res
-    
+
+@app.route('/remove')
+def removeCookie():
+    res = make_response("<h1>Removing the Cookie </h1>")
+    res.delete_cookie("theme")
+    return res
 
 if __name__=="__main__":
     app.run(port=3000,debug=True)
